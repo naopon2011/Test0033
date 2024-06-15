db177b9e0f4dc1fe8474caafd675eeb1cbfc0415
Kazuhiro Umemura <supernovatypebiz@gmail.com>
2024-06-15 12:44:27 +0900

    edit eip setting

--- main_aws.tf
+++ main_aws.tf
@@ -82,7 +82,7 @@ resource "aws_nat_gateway" "nat_gateway" {
 
 # Elastic IPの作成
 resource "aws_eip" "eip" {
-  domain = true
+  domain = "vpc"
   tags = {
     Name = "${var.aws_vpc_name}-eip"
     Tag = var.aws_vpc_name
